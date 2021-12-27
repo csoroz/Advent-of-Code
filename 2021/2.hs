cdd2c207c988d6251c6c0c240f1a6575424b1f98
csoroz <csoroz@gmail.com>
2021-12-27 23:47:05 +0100

    Add files via upload

--- 2021/2.hs
+++ 2021/2.hs
@@ -1,18 +1,18 @@
 data Cmd a = F a | D a | U a deriving Show
 
-(x,y) +# F v = (x+v,y)
-(x,y) +# D v = (x,y+v)
-(x,y) +# U v = (x,y-v)
+course (x,y) = x*y
 
-solve = p . foldl (+#) (0,0)
-  where p (a,b) = a*b
-
-solveAim = p . snd . foldl f (0,(0,0))
+advance = course . foldl (#>) (0,0)
   where
-    f (a,(x,y)) (F t) = (a,(x+t,y+a*t))
-    f (a,(x,y)) (D t) = (a+t,(x,y))
-    f (a,(x,y)) (U t) = (a-t,(x,y))
-    p (x,y) = x*y
+    (x,y) #> F v = (x+v,y)
+    (x,y) #> D v = (x,y+v)
+    (x,y) #> U v = (x,y-v)
+
+aiming = course . snd . foldl (#>) (0,(0,0))
+  where
+    (a,(x,y)) #> F t = (a,(x+t,y+a*t))
+    (a,(x,y)) #> D t = (a+t,(x,y))
+    (a,(x,y)) #> U t = (a-t,(x,y))
 
 parse :: [String] -> Cmd Integer
 parse ["forward", x] = F (read x)
@@ -21,5 +21,5 @@ parse ["up"     , x] = U (read x)
 
 fork (f,g) x = (f x, g x)
 
-main = interact $ show . fork (solve,solveAim) . map p . lines
+main = interact $ show . fork (advance,aiming) . map p . lines
   where p = parse . words
