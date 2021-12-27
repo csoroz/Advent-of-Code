fe1d97df1e5f584a80596502bedd0df1fc5d9993
csoroz <csoroz@gmail.com>
2021-12-28 00:55:08 +0100

    Add files via upload

--- 2021/3.hs
+++ 2021/3.hs
@@ -16,6 +16,9 @@ most = foldl count (0,0)
     count (a,b) '0' = (a+1,b)
     count (a,b) '1' = (a,b+1)
 
+type Cmp a = a -> a -> Bool
+
+select :: Cmp Int -> (Int,Int) -> Char
 select cmp (a,b) = if cmp a b then '0' else '1'
 
 power :: [String] -> Integer
@@ -25,18 +28,20 @@ power = g  . map f . transpose
         epsilon = binary . map neg
         gamma   = binary
 
+rating :: Cmp Int -> [String] -> Integer
+rating cmp = go 0
+  where
+    go _ [x] = binary (digits x)
+    go n xs  = go (n+1) (f xs)
+      where f = filter ((==y).(!!n))
+            t = transpose xs !! n
+            y = select cmp (most t)
+
 support :: [String] -> Integer
 support xs = generator * scrubber
   where
-    generator = rating (>)
-    scrubber = rating (<=)
-    rating cmp = go 0 xs
-      where
-        go _ [x] = binary (digits x)
-        go n xs  = go (n+1) (f xs)
-          where f = filter ((==y).(!!n))
-                t = transpose xs !! n
-                y = select cmp (most t)
+    generator = rating (>) xs
+    scrubber = rating (<=) xs
 
 fork (f,g) x = (f x, g x)
 
