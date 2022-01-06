0abbf81a177d8f545decceb1de8c6cab308377c9
csoroz <csoroz@gmail.com>
2022-01-06 17:46:55 +0100

    Parts One $ Two

--- 2021/day12/Main.hs
+++ 2021/day12/Main.hs
@@ -1,45 +1,42 @@
+import qualified Data.Map.Strict as M
+import Data.Map.Strict (Map,(!))
 import Data.List
 import Data.List.Extra (groupSort)
 import Data.List.Split (splitOn)
-import Data.Char (isLower)
-import Data.Maybe (fromJust)
+import Data.Char (isUpper)
 import Data.Tuple (swap)
 
 type Node = String
 type Path = [Node]
 type Edge = (Node,Node)
-type Graph = [(Node,[Node])]
+type Graph = Map Node [Node]
 
-small :: Node -> Bool
-small = isLower . head
+graph :: Node -> Node -> [Edge] -> Graph
+graph start end es = M.fromList $ canon $ groupSort $ f (es ++ map swap es)
+  where f = filter (\(a,b) -> a /= end && b /= start)
+        canon = map (fmap sort) -- optional
 
-nodes :: Graph -> [Node]
-nodes = map fst
+paths :: Int -> Node -> Node -> Graph -> [Path]
+paths n start end g = go start (n, M.fromList [(t,0) | t <- smalls])
+  where smalls = filter (not.big) (M.keys g) \\ [start]
+        big = isUpper . head
+        go a (k,vs)
+          | a == end = [[a]]
+          | otherwise = [a:p | t <- g!a
+                             , let (check,kvs) = visit t
+                             , check, p <- go t kvs]
+          where visit t | t == end    = (True , (k,vs))
+                        | big t       = (True , (k,vs))
+                        | v <  k      = (True , (k,vs'))
+                        | v == k      = (True , (1,vs'))
+                        | otherwise   = (False, (1,vs'))
+                        where (v,vs') = (vs!t + 1, M.insert t v vs)
 
-succs :: Node -> Graph -> [Node]
-succs a = fromJust . lookup a
+count n = length . paths n "start" "end"
 
-graph :: [Edge] -> Graph
-graph es = map (fmap sort) $ groupSort $ map swap es ++ es
-
-prune :: Node -> Graph -> Graph
-prune a = map (fmap (delete a)) . deleteNode a
-
-deleteNode :: Node -> Graph -> Graph
-deleteNode a = deleteBy f (a,[])
-  where f x y = fst x == fst y
-
-paths :: Node -> Node -> Graph -> [Path]
-paths start end g = go start g
-  where
-    go a g
-      | a == end = [[a]]
-      | otherwise = [a:p | t <- succs a g, p <- go t g']
-      where g' = if small a then prune a g else g
-
-count = length . paths "start" "end"
-
-parse = graph . map (pair . splitOn "-")
+parse = graph "start" "end" . map (pair . splitOn "-")
   where pair [a,b] = (a,b)
 
-main = interact $ show . count . parse . lines
+fork (f,g) x = (f x, g x)
+
+main = interact $ show . fork (count 1, count 2) . parse . lines
